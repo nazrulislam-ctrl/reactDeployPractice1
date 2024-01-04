51ef85a83346b595f393bd9d05620a5d620a8139
Md. Nazrul Islam <85072417+nazrulislam-ctrl@users.noreply.github.com>
2024-01-05 03:48:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment=()=> {
+    setCounter(counter+1);
+  }
+  const decrement=()=> {
+    if(counter>0){
+      setCounter(counter-1);
+    }
+  }
+  const reset=()=> {
+    setCounter(0);
+  }
+
+  return (
+    <>
+    <div className="container mx-auto flex items-center justify-center h-[100px] bg-green-600 gap-9 {counter}">
+      <button  className='btn'>Counter {counter}</button>
+      <button  className='btn'>Counter {counter}</button>
+      <button  className='btn'>Counter {counter}</button>
+      <button  className='btn'>Counter {counter}</button>
+    </div>
+    <div className="container mx-auto flex items-center justify-center h-[100px] gap-4">
+      <button onClick={increment} className='btn btn-success'>Increase</button>
+      <button onClick={decrement} className='btn bg-red-700'>Decrease</button>
+      <button onClick={reset} className='btn bg-yellow-700'>Reset Counter</button>
+    </div>
+    
+    </>
+  )
+}
+
+export default App;
